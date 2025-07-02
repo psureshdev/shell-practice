f17c94e1c6377f7dde8340f2501b19215608e1d3
suresh <psuresh336@gmail.com>
2025-07-02 21:12:01 +0530

    helloworld change6

--- 01.helloworld.sh
+++ 01.helloworld.sh
@@ -4,9 +4,9 @@ person1=suresh
 person2=giri
 
 echo "hello world"
-echo "person1"
-echo "person1 : hi"
-echo "person2 : hi"
-echo "person1 : pora"
-echo "person2 : ok"
+echo "$person1"
+echo "$person1 : hi"
+echo "$person2 : hi"
+echo "$person1 : pora"
+echo "$person2 : ok"
 
