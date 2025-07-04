7a43c3695189f000d24b0138359c4c6a6654f4b2
suresh <psuresh336@gmail.com>
2025-07-04 14:38:40 +0530

    k

---
+++ fun2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash/
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then 
+    echo "error: pls run with root user"
+    exit 1
+else
+   echo "installing with root user"
+fi
+
+#tree
+dnf list installed tree
+if [ $? -ne 0 ]
+then 
+  echo "tree not installed"
+else
+  echo "tree installed"
+  exit 1
+  dnf install tree -y
+fi
+
+if [ $? -eq 0 ]
+then 
+    echo "tree success"
+else
+   echo "tree failure"
+   exit 1
+fi
