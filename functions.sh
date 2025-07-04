d1878fe0703c8f9718b6cf05531671e132b81d5a
suresh <psuresh336@gmail.com>
2025-07-04 16:21:58 +0530

    k

--- functions.sh
+++ functions.sh
@@ -10,23 +10,24 @@ else
    echo "installing with root user"
 fi
 
+#validate
+validate (){
+    if [ $1 -eq 0 ]
+    then
+       echo "$2 is success"
+    else
+       echo "$2 failure"
+       exit 1
+    fi
+}
+
 #tree
 dnf list installed tree
 if [ $? -ne 0 ]
 then 
   echo "tree not installed"
 dnf install tree -y
-else
-  echo "tree installed"
-  exit 1
-fi
-
-if [ $? -eq 0 ]
-then 
-    echo "tree success"
-else
-   echo "tree failure"
-   exit 1
+validate $? tree
 fi
 
 #nginx
@@ -35,17 +36,7 @@ if [ $? -ne 0 ]
 then 
   echo "nginx not installed"
 dnf install nginx -y
-else
-  echo "nginx installed"
-  exit 1
-fi
-
-if [ $? -eq 0 ]
-then 
-    echo "nginx success"
-else
-   echo "nginx failure"
-   exit 1
+validate $? nginx
 fi
 
 #java
@@ -54,15 +45,5 @@ if [ $? -ne 0 ]
 then 
   echo "java not installed"
 dnf install java -y
-else
-  echo "java installed"
-  exit 1
-fi
-
-if [ $? -eq 0 ]
-then 
-    echo "java success"
-else
-   echo "java failure"
-   exit 1
+validate $? tree
 fi
