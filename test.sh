fb9bf8b015a056775687d2040ad678b93d26a7d0
suresh <psuresh336@gmail.com>
2025-07-04 17:12:10 +0530

    k

--- test.sh
+++ test.sh
@@ -11,5 +11,24 @@ else
     exit 1
 fi
 
+#validate function
+validate (){
+if [ $1 -eq 0 ]
+then 
+    echo "$2 is installed"
+else 
+    echo "$2 is failure"
+    exit 1
+fi
+}
+
 #tree installing
 dnf list installed tree
+if [ $? -ne 0 ]
+then
+    echo "tree is not installed"
+    dnf install tree -y
+    validate $? tree
+else 
+   echo "tree is installed"
+fi
