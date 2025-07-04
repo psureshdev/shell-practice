76251164dece6bec15645878e4381ca3d65d1fba
suresh <psuresh336@gmail.com>
2025-07-04 16:59:40 +0530

    k

--- test.sh
+++ test.sh
@@ -3,10 +3,13 @@
 #validating root user
 userid=$(id -u)
 
-if [ $userid -eq 0]
+if [ $userid -eq 0 ]
 then
     echo "running with root user"
 else 
     echo "pls run with root user"
     exit 1
 fi
+
+#tree installing
+dnf list installed trees
